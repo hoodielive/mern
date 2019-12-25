5b832f19405e9c2e9bed7c5c9fc9d66e0116cf3e
hoodielive <hoodielive@gmail.com>
2019-12-25 15:27:08 -0500

    User Registration
    User can now registerate to service. There is a validation put in place
    to ensure that user provides all required information.
    Bcrypt is our hashing mechanism for password security.

--- devconnector/config/db.js
+++ devconnector/config/db.js
@@ -6,7 +6,9 @@ const db = config.get('mongoURI')
 const connectDB = async () => {
     try {
 			await mongoose.connect(db, {
-				 useNewUrlParser: true
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+				createIndexes: true,
 			}) 
        console.log('MongoDB connected...')
     }
