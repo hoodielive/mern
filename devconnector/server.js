99f2393c4e5fa9838bcb7c62dca4e1c1444ddc0c
hoodielive <hoodielive@gmail.com>
2019-12-25 14:19:10 -0500

    This is where I created the routes for: 1. Users 2. Auth 3. Profiles 4. Posts

--- devconnector/server.js
+++ devconnector/server.js
@@ -10,6 +10,12 @@ connectDB()
 // Routes.
 app.get('/', (req, res) => res.send('API Running...'))
 
+// Define Routes.
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
 // Port Configuration.
 const PORT = process.env.PORT || 5000
 
