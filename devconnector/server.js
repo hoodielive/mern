0bdfcb41e941c009a3cebc3b44254d2dcf3eda73
hoodielive <hoodielive@gmail.com>
2019-12-25 13:15:34 -0500

    Mongo/Mongoose Database configuration

--- devconnector/server.js
+++ devconnector/server.js
@@ -1,8 +1,17 @@
+// Environmental Variables
 const express = require('express')
+const connectDB = require('./config/db')
 const app = express()
 
+// Connect Database.
+
+connectDB()
+
+// Routes.
 app.get('/', (req, res) => res.send('API Running...'))
 
+// Port Configuration.
 const PORT = process.env.PORT || 5000
 
+// HTTP Server Configuration.
 app.listen(PORT, () => console.log(`Awesome Server started on port ${PORT}....`))
