2284e1c0e572817846a9a956820a3d3a1b0aa217
hoodielive <hoodielive@gmail.com>
2019-12-25 17:30:21 -0500

    Added jwtToken configuration

--- devconnector/routes/api/users.js
+++ devconnector/routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const { check, validationResult } = require('express-validator')
 
 // Pull in User Model.
@@ -60,7 +62,23 @@ async (req, res) => {
 		await user.save()
 
 		// Return jsonwebtoken.
-		res.send('User registered.') 
+		const payload = {
+			user: {
+				id: user.id
+			}
+		}
+
+		jwt.sign(
+			payload, 
+			config.get('jwtSecret'),
+			{ 
+				expiresIn: 360000 
+			},
+			(error, token) => {
+				if (err) throw err
+				res.json({ token })
+			}
+		)
 	}
 	catch (err) {
 		console.error(err.message)
