b293264f5dd858f7e3d142696cef6419473c1ba9
hoodielive <hoodielive@gmail.com>
2019-12-26 18:26:43 -0500

    I was a bit fuzzy on jwt.
    I have created this project to practice using the jwt (json web token)
    library. I get it now.

--- devconnector/routes/api/auth.js
+++ devconnector/routes/api/auth.js
@@ -3,9 +3,11 @@ const router = express.Router()
 const auth = require('../../middleware/auth')
 
 const User = require('../../models/User')
+
 // @route  GET api/auth
 // @desc   Test route
 // @access Public
+
 router.get('/', auth, async (req, res) =>  {
 	try {
 		const user = await User.findById(req.user.id).select('-password')
