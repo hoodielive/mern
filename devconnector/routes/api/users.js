f8dec21830dda6bf68d680ecbfd48e40fe8109df
hoodielive <hoodielive@gmail.com>
2019-12-25 21:29:42 -0500

    This jwt configuration will not return the damn token. What am I doing wrong?

--- devconnector/routes/api/users.js
+++ devconnector/routes/api/users.js
@@ -12,11 +12,13 @@ const User = require('../../models/User')
 // @route  POST api/users
 // @desc   Register User
 // @access Public
+
 router.post('/', [
 	check('name', 'Name is required.')
 		.not()
 		.isEmpty(),
-	check('email', 'Please include a valid email.').isEmail(),
+	check('email', 'Please include a valid email.')
+		.isEmail(),
 	check(
 		'password',
 		'Please enter a password with 6 or more characters'
@@ -72,7 +74,7 @@ async (req, res) => {
 			payload, 
 			config.get('jwtSecret'),
 			{ 
-				expiresIn: 360000 
+				expiresIn: 3600
 			},
 			(error, token) => {
 				if (err) throw err
