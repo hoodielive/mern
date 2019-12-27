41694b10c7593c44921c37f2d6d0559a713adb2c
hoodielive <hoodielive@gmail.com>
2019-12-27 17:29:30 -0500

    Added Logic for Profile Schema.
    The Application can do some validation relative to the Schema.

--- devconnector/routes/api/profile.js
+++ devconnector/routes/api/profile.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const auth = require('../../middleware/auth')
 const Profile = require('../../models/Profile')
 const User = require('../../models/User')
+const { check, validationResult } = require('express-validator')
 
 // @route  GET api/profile/me
 // @desc   Get current users profile
@@ -24,5 +25,65 @@ router.get('/me', auth, async (req, res) => {
         res.status(500).json({ message: 'Server error.'})
     }
 })
+
+// @route  GET api/profile
+// @desc   Create or update user profile
+// @access Private
+
+router.post('/', 
+[
+    auth, 
+    [
+        check('status', 'Status is required.').not().isEmpty(),
+        check('skills', 'Skills is required.').not().isEmpty(),
+    ] 
+],
+    async (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        const {
+            company,
+            website,
+            location,
+            bio,
+            status,
+            githubusername,
+            skills, 
+            youtube,
+            facebook,
+            twitter,
+            instagram,
+            linkedin
+        } = req.body
+
+        // Build profile object 
+        const profileFields = {}
+        profileFields.user = req.user.id
+
+        if (company) profileFields.company = company
+        if (website) profileFields.website = website
+        if (location) profileFields.location = location
+        if (bio) profileFields.bio = bio
+        if (status) profileFields.status = status
+        if (githubusername) profileFields.githubusername = githubusername
+
+        if (skills) {
+            profileFields.skills = skills.split(',').map(skill => skill.trim())
+        }
+
+        profileFields.social = {}
+        if (youtube) profileFields.youtube = youtube
+        if (twitter) profileFields.twitter = twitter
+        if (facebook) profileFields.facebook = facebook
+        if (linkedin) profileFields.linkedin = linkedin
+        if (instagram) profileFields.instagram = instagram
+
+        console.log(profileFields.social.twitter)
+        res.send('Hello')
+    }
+)
 
 module.exports = router;
