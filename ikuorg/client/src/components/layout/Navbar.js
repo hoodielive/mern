ffc7f481f10e7ee913e6869d25dabf46ccfd003e
hoodielive <hoodielive@gmail.com>
2020-02-22 11:49:44 -0500

    unfix fix unfix

--- ikuorg/client/src/components/layout/Navbar.js
+++ ikuorg/client/src/components/layout/Navbar.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import {
-    Link
-} from 'react-router-dom'
-import M from 'materialize-css'
-
 const Navbar = () => {
-    return ( 
-        <nav>
-            <div class="nav-wrapper">
-            <a href="#" class="right hide-on-med-and-down">
-                <li><a href="#">About</a></li>
-                <li><a href="#">Register</a></li>
-                <li><a href="#">Login</a></li>
-        </nav>
+    return (
+      <nav className="navbar bg-dark">
+        <h1>
+        </h1>
+        <ul>
+            <li>
+                <Link to="!#">About</Link>
+            </li>
+            <li>
+                <Link to="/register">Register</Link>
+            </li>
+            <li>
+                <Link to="/login">Login</Link>
+            </li>
+        </ul>
+     </nav>
     )
 }
-export default Navbar;
